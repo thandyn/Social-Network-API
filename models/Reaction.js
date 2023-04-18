68a1b989cc3f8d6d238f36c50e6d3fd804731490
thandyn <nguyenphuthan@gmail.com>
2023-04-17 18:17:20 -0700

    added reaction.js model, needs createdAt

---
+++ models/Reaction.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId(),
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    //do later
+  },
+});
+
+module.exports = reactionSchema;
