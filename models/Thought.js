3b26db55e29379138cfa229bd2cc9f40ea68c604
thandyn <nguyenphuthan@gmail.com>
2023-04-17 18:03:48 -0700

    created Thought.js model, needs dateformat

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String, 
+      required: true, 
+      minlenght: 1,
+      maxlength: 280, 
+    },
+    createdAt: {
+      type: Date, 
+      default: Date.now,
+      get: 
+    },
+    username: {
+      type: String, 
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters
+    }
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
